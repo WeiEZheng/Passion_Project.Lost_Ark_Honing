1c0533a53bf477ef2c2a37aa2b55a766cebc73c5
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-20 10:58:16 -0400

    Delete LostArk directory
    rebasing

--- LostArk/src/main/webapp/app/shared/model/item.model.ts
+++
@@ -1,8 +0,0 @@
-import { MaterialName } from 'app/shared/model/enumerations/material-name.model';
-
-export interface IItem {
-  id?: number;
-  itemName?: MaterialName | null;
-}
-
-export const defaultValue: Readonly<IItem> = {};
