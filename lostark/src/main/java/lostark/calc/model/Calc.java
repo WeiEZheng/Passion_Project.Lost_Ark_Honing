f9890c8d88137d3f04a2ef8969dbb248e0b3aa5a
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-24 12:25:40 -0400

    wrote some logic for the calculation portion

--- lostark/src/main/java/lostark/calc/model/Calc.java
+++ lostark/src/main/java/lostark/calc/model/Calc.java
@@ -1,6 +1,9 @@
 package lostark.calc.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import lostark.domain.Equipment;
 import lostark.domain.MarketPrice;
 import lostark.domain.enumeration.EquipType;
@@ -15,6 +18,28 @@ public class Calc {
 
     @Autowired
     static MarketPriceServiceImpl marketPriceService;
+
+    public static double compareCost(Equipment equipment, Double basePercent, Double additionPercentPerFail,
+                                     Integer failLimit, Double maxPercentAfterMats,
+                                     Integer fusionMat1Amount, Integer fusionMat2Amount, Integer fusionMat3Amount){
+        Integer cost = RegularCost(equipment);
+        Double waitTimeDiff = simulate(basePercent, additionPercentPerFail, failLimit, 100000)
+            - simulate(maxPercentAfterMats, additionPercentPerFail, failLimit, 100000);
+        Integer costOfFusionMat = getExtraCost(equipment, fusionMat1Amount, fusionMat2Amount, fusionMat3Amount);
+        return waitTimeDiff*cost-costOfFusionMat;
+    }
+
+    public static Integer getExtraCost(Equipment equipment, Integer fusionMat1Amount,
+                                       Integer fusionMat2Amount, Integer fusionMat3Amount) {
+        if (equipment.getTier()==TierEnum.Tier1)
+            return getCost(fusionMat1Amount, marketPriceService.findOneByItemName(MaterialName.StarsBreaths).get());
+        else if (equipment.getTier()==TierEnum.Tier2)
+            return getCost(fusionMat1Amount, marketPriceService.findOneByItemName(MaterialName.MoonsBreaths).get());
+        else
+            return getCost(fusionMat1Amount, marketPriceService.findOneByItemName(MaterialName.SolarGrace).get())
+                +getCost(fusionMat2Amount, marketPriceService.findOneByItemName(MaterialName.SolarBlessing).get())
+                +getCost(fusionMat3Amount, marketPriceService.findOneByItemName(MaterialName.SolarProtection).get());
+    }
 
     public static Integer RegularCost(Equipment equipment) {
         List<Integer> mat = honingMat.get(equipment.getEquipmentType(), equipment.getTier(), equipment.getHoningLevel(), false);
@@ -75,5 +100,26 @@ public class Calc {
 
     public static Integer getCost(Integer count, MarketPrice marketPrice) {
         return ((int) Math.ceil(1.0 * count / marketPrice.getNumberPerStack())) * marketPrice.getItemPricePerStack();
+    }
+
+    public static double simulate(Double basePercent, Double additionPercentPerFail, Integer failLimit, Integer SimCount){
+        Random random = new Random();
+        Integer count = 0;
+        List<Integer> waitTime= new ArrayList<>();
+        while (true){
+            if (random.nextDouble(1)<=(basePercent+(count*additionPercentPerFail)) || count>failLimit){
+                waitTime.add(count+1);
+                count=0;
+                continue;
+            }
+            count++;
+            if (waitTime.size()>=SimCount)
+                break;
+        }
+        double avg = 0.0;
+        for (int i=0; i<waitTime.size();i++){
+            avg += Double.valueOf((waitTime.get(i) - avg) / (i + 1));
+        }
+        return avg;
     }
 }
--- lostark/src/main/java/lostark/calc/model/EffResponse.java
+++ lostark/src/main/java/lostark/calc/model/EffResponse.java
@@ -1,3 +1,27 @@
 package lostark.calc.model;
 
-public class EffResponse {}
+public class EffResponse {
+    private Double amountDiff;
+
+    public EffResponse() {
+    }
+
+    public EffResponse(Double amountDiff) {
+        this.amountDiff=amountDiff;
+    }
+
+    public Double getAmountDiff() {
+        return amountDiff;
+    }
+
+    public void setAmountDiff(Double amountDiff) {
+        this.amountDiff = amountDiff;
+    }
+
+    @Override
+    public String toString() {
+        return "EffResponse{" +
+            "amountDiff=" + amountDiff +
+            '}';
+    }
+}
